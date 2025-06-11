6adf1414f2c2157fa02067064a9cb408d7e97648
Matang Mehra <codewritter.1@gmail.com>
2025-06-12 03:28:26 +0530

    Updated Readme.md and Yt-Spoof.sh

--- Yt_Spoof.sh
+++ Yt_Spoof.sh
@@ -16,6 +16,16 @@ getSpotifyAudio() {
 spotify_dl -l $1
 }
 
+echo " __   __  ______        ____    ____    _____   _____   ____   "
+echo "/\ \ /\ \/\__ _\      /\ _\`\ /\ _\`\ /\ __\`\/\ __\`\/\ _\`\  "
+echo "\ \`\`\\/ /\/_/\ \/      \ \,\L\_\ \ \L\ \ \ \/\ \ \ \/\ \ \ \L\_\ "
+echo " \ \`\ /   \ \ \        \/_\__ \\ \ ,__/\ \ \ \ \ \ \ \ \ \ _\/ "
+echo "  \`\ \ \   \ \ \          /\ \L\ \ \ \/  \ \ \_\ \ \ \_\ \ \/  "
+echo "    \ \_\   \ \_\          \`\____\ \_\   \ \_____\ \_____\ \_\  "
+echo "     \/_/    \/_/            \/_____/\/_/    \/_____/\/_____/\/_/  "
+echo "    Created by : @mgmehra2005        Follow on Git"
+echo ""
+
 echo 1. Youtube
 echo 2. Spotify
 
@@ -23,17 +33,23 @@ read -p ">> " choice
 clear
 
 if [[ $choice -eq 1 ]]; then
+	echo "Selected Platform Youtube"
+	echo ""
 	echo 1. Audio
 	echo 2. Video
 	read -p ">> " yt_choice
 	clear
 
 	if [[ $yt_choice -eq 1 ]]; then
+		echo "Selected Platform Youtube Downlaoding Audio"
+		echo ""
 		read -p "Audio File Link : " link
 		getYoutubeAudio $link
 
 	elif [[ $yt_choice -eq 2 ]]; then
-		read -p "Video File Linke : " link
+		echo "Selected Platform Youtube Downlaoding Video"
+		echo ""
+		read -p "Video File Link : " link
 		getYoutubeVideo $link
 
 	else
@@ -42,8 +58,11 @@ if [[ $choice -eq 1 ]]; then
 	fi
 
 elif [[ $choice -eq 2 ]]; then
+	echo "Selected Platform Spotify Downlaoding Audio"
+	echo ""
 	read -p "Audio File Link : " link
 	getSpotifyAudio $link
 else
 	echo Wrong User Input
+	exit 0
 fi
